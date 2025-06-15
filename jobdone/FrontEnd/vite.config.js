be2bd5c2b2b27b9543d05e42220d36edade20928
rounak <molpariyarounak@gmail.com>
2025-06-15 23:42:52 +0530

    new file:   jobdone/FrontEnd/vercel.json 	modified:   jobdone/FrontEnd/vite.config.js

--- jobdone/FrontEnd/vite.config.js
+++ jobdone/FrontEnd/vite.config.js
@@ -4,35 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy:{
-      "/user": {
-        target: "https://jobdone-zh3o.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/posts": {
-        target: "https://jobdone-zh3o.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/api": {
-        target: "https://jobdone-zh3o.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/conversations": {
-        target: "https://jobdone-zh3o.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/auth": {
-        target: "https://jobdone-zh3o.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      }
-    },
-  },
+  base: "/",
   plugins: [
     tailwindcss(),
     react()
