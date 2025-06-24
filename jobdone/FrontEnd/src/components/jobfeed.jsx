731a6121d20b8272d83fc9ccd0b2cc0e7fc4a42a
rounak <molpariyarounak@gmail.com>
2025-06-24 22:30:36 +0530

    modified:   jobdone/FrontEnd/src/components/jobfeed.jsx 	modified:   jobdone/FrontEnd/src/userJobs.jsx

--- jobdone/FrontEnd/src/components/jobfeed.jsx
+++ jobdone/FrontEnd/src/components/jobfeed.jsx
@@ -87,7 +87,7 @@ function JobFeed({ refreshFlag  }) {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center h-screen bg-white">
+      <div className="flex items-center justify-center mt-40 sm:mt-10 lg:mt-30 bg-white">
         <div className="w-12 h-12 border-4 border-teal-500 border-dashed rounded-full animate-spin"></div>
       </div>
     );
