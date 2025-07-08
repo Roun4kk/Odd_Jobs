6add8f789312168d9b807c8e56ee64c3e325c455
rounak <molpariyarounak@gmail.com>
2025-07-09 01:52:38 +0530

    modified:   jobdone/FrontEnd/src/components/bidSection.jsx

--- jobdone/FrontEnd/src/components/bidSection.jsx
+++ jobdone/FrontEnd/src/components/bidSection.jsx
@@ -169,7 +169,7 @@ function BidSection({ postId, refresh, sortBy, currentUserId, jobPosterId , post
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center h-screen bg-white">
+      <div className="flex justify-center mt-25 h-screen bg-white">
         <div className="w-12 h-12 border-4 border-teal-500 border-dashed rounded-full animate-spin"></div>
       </div>
     );
